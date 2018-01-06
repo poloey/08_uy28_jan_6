28aac57145c17c6aacec23c6c1810242005a3824
polo dev <polodev10@gmail.com>
2018-01-06 16:32:12 +0600

    added class 8

---
+++ script.js
@@ -0,0 +1,93 @@
+///// array and object
+// var fruits1 = ['apple', 'orange', 'banana'];
+// var fruits2 = {f1: 'apple', f2: 'orange', f3: 'banana'};
+// fruits1[0]
+// fruits2['f1']
+// fruits2.f1
+///// if else
+// var age = 10;
+// if ( age < 18) {
+//   console.log('He is not a voter')
+// } else {
+//   console.log('He is a voter')
+// }
+
+/////ternary
+ // age < 18 ? console.log('He is not a voter') : console.log('He is a voter')
+
+/// if else if else
+var favorite_fruit = 'mango';
+// if (favorite_fruit === 'guava') {
+//   console.log('My favorite fruit is guava');
+// } else if (favorite_fruit === 'jack fruit') {
+//   console.log('My favorite fruit is jack-fruit');
+// } else {
+//   console.log('My favorite fruit is Mango');
+// }
+
+/// switch 
+/// 
+// switch(favorite_fruit) {
+//   case 'guava':
+//     console.log('My favorite fruit is guava');
+//     break;
+//   case 'jack-fruit':
+//     console.log('My favorite fruit is jack-fruit');
+//     break;
+//   default:
+//     console.log('My favorite fruit is Mango');
+// }
+
+/// iterative 
+// var number = 0;
+// while (number < 11) {
+//   console.log(number);
+//   number++;
+// }
+
+
+//// for loop
+// for (var number = 0; number < 11; number++) {
+//   console.log(number);
+// }
+
+// var number = 0;
+// do {
+//   console.log(number);
+//   number++;
+// }while (number > 11) 
+
+// built in function 
+
+// var fruit = 'apple';
+// console.log(fruit.toUpperCase());
+
+function add (n1, n2) {
+  return n1 + n2;
+}
+console.log(add(2, 4));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
